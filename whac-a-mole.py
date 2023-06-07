b89e97f1c5f8a5ba58b6410aa0936a30d5306741
WooML <127821186+WooML@users.noreply.github.com>
2023-06-07 16:01:30 +0900

    fix tkinter to pyqt5

--- whac-a-mole.py
+++ whac-a-mole.py
@@ -1,235 +1,84 @@
-# -*- coding: utf-8 -*-
-from tkinter import *
-from time import *
-import threading
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt, QTimer
 import random
 
-whacks = 0
-hole = 0
+class WhacAMoleGame(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.whacks = 0
+        self.hole = 0
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.whac_a_mole)
+        
+        self.setWindowTitle("Whac-A-Mole")
+        self.setStyleSheet("background-color: green;")
+        self.setFixedSize(500, 520)
+        self.setWindowFlags(Qt.WindowStaysOnTopHint)
 
-screen = Tk()
-screen.config(bg="green", width=500, height=520)
-screen.resizable(0, 0)
-screen.attributes('-topmost', True)
-tip = Label(bg='black', fg='green', text="Play with mouse for best experience")
-tip.place(x=0, y=497)
-lab = Label(text="WHAC-A-MOLE!", font=("Algerian", 30), bg='blue', fg='yellow')
-lab.place(x=110, y=0)
-scorelabel = Label(text=whacks, width=7, font=('Bahnschrift', 30), bg='yellow')
-scorelabel.place(x=170, y=52)
-remark = Label(text="", width=17, bg='red', fg='yellow', font="Centaur 20")
-remark.place(x=130, y=107)
+        self.tip_label = QLabel("Play with mouse for best experience", self)
+        self.tip_label.setStyleSheet("background-color: black; color: green;")
+        self.tip_label.setGeometry(0, 497, 500, 23)
 
+        self.lab_label = QLabel("WHAC-A-MOLE!", self)
+        self.lab_label.setFont(QFont("Algerian", 30))
+        self.lab_label.setStyleSheet("background-color: blue; color: yellow;")
+        self.lab_label.setGeometry(110, 0, 280, 50)
 
-def onwhack():
-    global hole
-    if hole == 1:
-        hole1.config(state='disabled', text='*_*', bg='red', fg='black')
-    elif hole == 2:
-        hole2.config(state='disabled', text='*_*', bg='red', fg='black')
-    elif hole == 3:
-        hole3.config(state='disabled', text='*_*', bg='red', fg='black')
-    elif hole == 4:
-        hole4.config(state='disabled', text='*_*', bg='red', fg='black')
-    elif hole == 5:
-        hole5.config(state='disabled', text='*_*', bg='red', fg='black')
-    elif hole == 6:
-        hole6.config(state='disabled', text='*_*', bg='red', fg='black')
-    elif hole == 7:
-        hole7.config(state='disabled', text='*_*', bg='red', fg='black')
-    elif hole == 8:
-        hole8.config(state='disabled', text='*_*', bg='red', fg='black')
-    elif hole == 9:
-        hole9.config(state='disabled', text='*_*', bg='red', fg='black')
-    global whacks
-    whacks += 1
-    scorelabel.config(text="* " + str(whacks) + " *")
+        self.score_label = QLabel(str(self.whacks), self)
+        self.score_label.setFont(QFont('Bahnschrift', 30))
+        self.score_label.setStyleSheet("background-color: yellow;")
+        self.score_label.setGeometry(170, 52, 160, 38)
+        self.score_label.move(170, 52)
 
+        self.remark_label = QLabel("", self)
+        self.remark_label.setFont(QFont("Centaur", 20))
+        self.remark_label.setStyleSheet("background-color: red; color: yellow;")
+        self.remark_label.setGeometry(130, 107, 220, 28)
 
-hole1 = Button(
-    width=6,
-    height=3,
-    bg='black',
-    state='disabled',
-    command=onwhack)
-hole1.place(x=20, y=160)
-hole2 = Button(
-    width=6,
-    height=3,
-    bg='black',
-    state='disabled',
-    command=onwhack)
-hole2.place(x=20, y=290)
-hole3 = Button(
-    width=6,
-    height=3,
-    bg='black',
-    state='disabled',
-    command=onwhack)
-hole3.place(x=20, y=420)
-hole4 = Button(
-    width=6,
-    height=3,
-    bg='black',
-    state='disabled',
-    command=onwhack)
-hole4.place(x=220, y=160)
-hole5 = Button(
-    width=6,
-    height=3,
-    bg='black',
-    state='disabled',
-    command=onwhack)
-hole5.place(x=220, y=290)
-hole6 = Button(
-    width=6,
-    height=3,
-    bg='black',
-    state='disabled',
-    command=onwhack)
-hole6.place(x=220, y=420)
-hole7 = Button(
-    width=6,
-    height=3,
-    bg='black',
-    state='disabled',
-    command=onwhack)
-hole7.place(x=420, y=160)
-hole8 = Button(
-    width=6,
-    height=3,
-    bg='black',
-    state='disabled',
-    command=onwhack)
-hole8.place(x=420, y=290)
-hole9 = Button(
-    width=6,
-    height=3,
-    bg='black',
-    state='disabled',
-    command=onwhack)
-hole9.place(x=420, y=420)
+        self.holes=[]
+        for i in range(9):
+            hole = QPushButton(self)
+            hole.setGeometry((i % 3) * 200 + 20, (i // 3) * 130 + 160, 90, 60)
+            hole.setStyleSheet("background-color: black;")
+            hole.setEnabled(False)
+            hole.clicked.connect(lambda checked, hole_num=i+1: self.on_whack(hole_num))
+            self.holes.append(hole)
+        
+        self.show()
 
 
-def start():
-    global whacks
-    whacks = 0
-    remark.config(text="")
-    replaybtn.config(state='disabled')
-    t = threading.Thread(target=whac_a_mole)
-    t.start()
+    def on_whack(self, hole_num):
+        if self.hole == hole_num:
+            hole = self.holes[hole_num - 1]
+            hole.setEnabled(False)
+            hole.setText('_')
+            hole.setStyleSheet("background-color: red; color: black;")
+            self.whacks += 1
+            self.score_label.setText(str(self.whacks))
+
+    def whac_a_mole(self):
+        if self.hole != 0:
+            hole = self.holes[self.hole - 1]
+            hole.setEnabled(False)
+            hole.setText('_')
+            hole.setStyleSheet("background-color: black;")
 
 
-def whac_a_mole():
-    ready_set_whack()
-    global hole
-    for i in range(0, 60):
-        hole = random.randint(1, 9)
-        if hole == 1:
-            hole1.config(
-                state='normal',
-                text='【•】__【•】',
-                bg='pink',
-                fg='black')
-            sleep(1)
-            hole1.config(state='disabled', text='', bg='black')
-        elif hole == 2:
-            hole2.config(
-                state='normal',
-                text='【•】__【•】',
-                bg='pink',
-                fg='black')
-            sleep(1)
-            hole2.config(state='disabled', text='', bg='black')
-        elif hole == 3:
-            hole3.config(
-                state='normal',
-                text='【•】__【•】',
-                bg='pink',
-                fg='black')
-            sleep(1)
-            hole3.config(state='disabled', text='', bg='black')
-        elif hole == 4:
-            hole4.config(
-                state='normal',
-                text='【•】__【•】',
-                bg='pink',
-                fg='black')
-            sleep(1)
-            hole4.config(state='disabled', text='', bg='black')
-        elif hole == 5:
-            hole5.config(
-                state='normal',
-                text='【•】__【•】',
-                bg='pink',
-                fg='black')
-            sleep(1)
-            hole5.config(state='disabled', text='', bg='black')
-        elif hole == 6:
-            hole6.config(
-                state='normal',
-                text='【•】__【•】',
-                bg='pink',
-                fg='black')
-            sleep(1)
-            hole6.config(state='disabled', text='', bg='black')
-        elif hole == 7:
-            hole7.config(
-                state='normal',
-                text='【•】__【•】',
-                bg='pink',
-                fg='black')
-            sleep(1)
-            hole7.config(state='disabled', text='', bg='black')
-        elif hole == 8:
-            hole8.config(
-                state='normal',
-                text='【•】__【•】',
-                bg='pink',
-                fg='black')
-            sleep(1)
-            hole8.config(state='disabled', text='', bg='black')
-        elif hole == 9:
-            hole9.config(
-                state='normal',
-                text='【•】__【•】',
-                bg='pink',
-                fg='black')
-            sleep(1)
-            hole9.config(state='disabled', text='', bg='black')
-    replaybtn.config(state='normal')
-    global whacks
-    if whacks < 10:
-        remark.config(text="POOR")
-    elif whacks >= 10 and whacks < 20:
-        remark.config(text="BAD")
-    elif whacks >= 20 and whacks < 30:
-        remark.config(text="DO BETTER!")
-    elif whacks >= 30 and whacks < 40:
-        remark.config(text="GOOD")
-    elif whacks >= 40 and whacks < 50:
-        remark.config(text="GREAT!")
-    elif whacks >= 50 and whacks < 60:
-        remark.config(text="AWESOME!")
-    elif whacks == 60:
-        remark.config(text="WHACKED 'em ALL!", font="Centaur 18")
+        self.hole = random.randint(1, 9)
+        hole = self.holes[self.hole - 1]
+        hole.setEnabled(True)
+        hole.setText('O')
+        hole.setStyleSheet("background-color: green; color: black;")
 
+    
+    def start_game(self):
+        self.whacks = 0
+        self.score_label.setText(str(self.whacks))
+        self.hole = 0
+        self.timer.start(1000)
 
-def ready_set_whack():
-    sleep(1)
-    scorelabel.config(text="Ready")
-    sleep(1)
-    scorelabel.config(text="Set")
-    sleep(1)
-    scorelabel.config(text="WHACK!!")
-    sleep(1)
-    scorelabel.config(text="0")
-
-
-replaybtn = Button(text="Play Again", command=start)
-replaybtn.place(x=0, y=2)
-
-start()
-
-
-screen.mainloop()
+app = QApplication([])
+game = WhacAMoleGame()
+game.start_game()
+app.exec_()
